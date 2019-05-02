abaf98010550f366387f5bd4bbeb711e4ebae742
qianyewoailuo <qianyewoailuo@126.com>
2019-05-02 06:27:17 +0000

    为heroku添加PostgreSQL

---
+++ app/helpers.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * @Description: 根据应用不同环境指定数据库配置信息
+ * @Author: luo
+ * @email: qianyewoailuo@126.com
+ * @Date: 2019-05-02 14:14:03
+ * @LastEditTime: 2019-05-02 14:15:48
+ */
+
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
